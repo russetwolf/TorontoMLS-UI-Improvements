7ce44a2f91f99c9939c46d3a4637186ffd6a01aa
Alex Cend <alexccend@gmail.com>
2020-06-26 22:26:49 -0400

    Refactoring into separate methods and building out listingObject from scraping the page.

--- TorontoMLS-UI-Improvements.user.js
+++ TorontoMLS-UI-Improvements.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         TorontoMLS.net UI Improvements
 // @namespace    http://tampermonkey.net/
-// @version      0.5
+// @version      0.6
 // @updateURL    https://github.com/russetwolf/TorontoMLS-UI-Improvements/raw/master/TorontoMLS-UI-Improvements.user.js
 // @downloadURL  https://github.com/russetwolf/TorontoMLS-UI-Improvements/raw/master/TorontoMLS-UI-Improvements.user.js
 // @description  Source and Liscence: https://github.com/russetwolf/TorontoMLS-UI-Improvements
@@ -14,10 +14,12 @@
   //boilerplate greasemonkey to wait until jQuery is defined...
   function GM_wait()
   {
-    if(typeof unsafeWindow.jQuery == 'undefined')
+    if(typeof unsafeWindow.jQuery == 'undefined') {
       window.setTimeout(GM_wait,100);
-    else
+    }
+    else {
       unsafeWindow.jQuery(function() { letsJQuery(unsafeWindow.jQuery); });
+    }
   }
   GM_wait();
 
@@ -25,37 +27,25 @@
   {
       'use strict';
 
-      makeTopLinkClickable($);
+      overallUIChanges($);
 
-      //Operate on each Info Sheet
-      var infoSheets = $('.report-container');
-      $.each(infoSheets, forEachInfoSheet);
+      //Operate on each Listing Sheet
+      var listings = $('.report-container');
+      $.each(listings, forEachListing);
 
-      function forEachInfoSheet(i, sheet) {
-          //Extract relevant info from sheet
-          var infoSheetBoxes = $(this).find('.form .legacyBorder div.formitem.formgroup.vertical');
-          var address = "";
-          $.each(infoSheetBoxes, selectRelevantBoxes);
+      function forEachListing(i, sheet) {
 
-          function selectRelevantBoxes(j, box) {
-            if(j === 3) {
-              // Extract address
-              address = box.innerText;
-              address = address.replace("\n", " ");
-            }
-            if(j === 1) {
-              // Placeholder select for photo boxes later
-              //$(this).css('background','#ccffff')
-              //console.log(box);
-            }
-          }
-          if(address != "") {
-            insertMapsLink($, i, sheet, removeUnitNumberFromAddress(address));
-          }
+          var listingObj = constructListingObject($, sheet);
+
+          perListingUIChanges($, listingObj, sheet);
       }
   }
 
-  function makeTopLinkClickable($) {
+  function overallUIChanges($) {
+    makeResultsClickableToListing($);
+  }
+
+  function makeResultsClickableToListing($) {
     var mlsNumCol = $('.data-list tbody td:last-child');
     $.each(mlsNumCol, addLink);
 
@@ -65,7 +55,80 @@
     }
   }
 
-  function insertMapsLink($, i, sheet, address) {
+  function constructListingObject($, sheet) {
+    var listingObj = {};
+    //Extract relevant info from sheet
+    var listingBoxes = $(sheet).find('.form .legacyBorder div.formitem.formgroup.vertical');
+    $.each(listingBoxes, parseListing);
+
+    function parseListing(j, box) {
+      if(j === 1) {
+        // Placeholder select for photo boxes later
+        //box.css('background','#ccffff')
+        //console.log(box);
+      }
+      if(j === 3) {
+        // Extract address
+        listingObj.address = box.innerText;
+        listingObj.address = listingObj.address.replace("\n", " ");
+      }
+      if(j === 5) {
+        // Extract price
+        listingObj.currentPrice = box.innerText;
+        listingObj.currentPrice = listingObj.currentPrice.split(" ")[0];
+        listingObj.currentPrice = listingObj.currentPrice.split("$")[1];
+        listingObj.currentPrice = parseFloat(listingObj.currentPrice.replace(/,/g, ''));
+      }
+      if(j === 9) {
+        // Extract type (Detatched, Condo, etc.)
+        listingObj.type = box.children[0].innerText;
+      }
+      if(j === 11) {
+        // Extract room numbers
+        listingObj.beds = box.children[1].innerText.split(":")[1];
+        listingObj.baths = box.children[2].innerText.split(":")[1];
+      }
+      if(j === 12) {
+        // Extract MLS#
+        listingObj.id = box.innerText;
+        listingObj.id = listingObj.id.split(":")[1];
+      }
+      if(j === 25) {
+        // Process left info column
+        $.each(box.children, selectRelevantLeftColChildren);
+
+        function selectRelevantLeftColChildren(k, child) {
+          var content = child.innerText.split(":");
+          if (content[0] === "Kitchens") {
+            listingObj.kitchens = 0;
+            var numKitchens = content[1].split(" + ");
+            for(var a = 0; a < numKitchens.length; a++) {
+              listingObj.kitchens += parseInt(numKitchens[a]);
+            }
+          }
+          if (content[0] === "Basement") {
+            listingObj.basement = content[1];
+          }
+          if (content[0] === "Apx Age") {
+            listingObj.age = content[1];
+          }
+          if (content[0] === "Apx Sqft") {
+            listingObj.sqft = content[1];
+          }
+        }
+      }
+    }
+    return listingObj;
+  }
+
+  function perListingUIChanges($, listingObj, sheet) {
+    if(listingObj.address) {
+      insertMapsLink($, sheet, util_removeUnitNumberFromAddress(listingObj.address));
+    }
+
+  }
+
+  function insertMapsLink($, sheet, address) {
     var ul = $(sheet).find('.links-container>ul');
     if (ul.length === 0 && ul.context.className === "report-container") {
         var linkContainer= $(sheet).find('.links-container');
@@ -78,7 +141,7 @@
     }
   }
 
-  function removeUnitNumberFromAddress(address) {
+  function util_removeUnitNumberFromAddress(address) {
     // separate postal code from rest of address, removing whitespace.
     var postcode = address.slice(-7).replace(/\s\s+/g, ' ');
     address = address.substring(0, address.length - 7).trimEnd();
