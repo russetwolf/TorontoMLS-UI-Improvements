7082ad48301a8389fed9a92c62c1f7e6ef192570
Alex Cend <alexccend@gmail.com>
2020-06-28 16:35:58 -0400

    Further improvements to scraping plus adding an alternate summary display. Still have errors with images. USes approximate hardcoded values for calculating mortgage cost and potential rent.

--- TorontoMLS-UI-Improvements.user.js
+++ TorontoMLS-UI-Improvements.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name         TorontoMLS.net UI Improvements
 // @namespace    http://tampermonkey.net/
-// @version      0.6
+// @version      0.7
 // @updateURL    https://github.com/russetwolf/TorontoMLS-UI-Improvements/raw/master/TorontoMLS-UI-Improvements.user.js
 // @downloadURL  https://github.com/russetwolf/TorontoMLS-UI-Improvements/raw/master/TorontoMLS-UI-Improvements.user.js
 // @description  Source and Liscence: https://github.com/russetwolf/TorontoMLS-UI-Improvements
 // @author       Alex Cendecki
 // @match        http://v3.torontomls.net/*
+// @grant        GM_addStyle
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js
 // ==/UserScript==
 
@@ -27,22 +28,25 @@
   {
       'use strict';
 
-      overallUIChanges($);
-
       //Operate on each Listing Sheet
       var listings = $('.report-container');
       $.each(listings, forEachListing);
 
       function forEachListing(i, sheet) {
-
+        //skip 0 as that's the list at the top and and even "listings" as the page has wierd shadow listings
+        if (i % 2 === 1) {
+          sheet.classList.add("reportSheet");
           var listingObj = constructListingObject($, sheet);
-
           perListingUIChanges($, listingObj, sheet);
+        }
       }
+
+      overallUIChanges($);
   }
 
   function overallUIChanges($) {
     makeResultsClickableToListing($);
+    addStickyNoteToggle($);
   }
 
   function makeResultsClickableToListing($) {
@@ -55,46 +59,90 @@
     }
   }
 
+  function addStickyNoteToggle($) {
+    var stickyToggle = document.createElement('div');
+    stickyToggle.id = "stickyToggleDiv";
+    stickyToggle.innerHTML = '<button id="stickyToggleButton" type="button">'
+                + 'Toggle Summary sheets (experimental)</button>'
+                ;
+    document.body.prepend(stickyToggle);
+    document.getElementById ("stickyToggleButton").addEventListener (
+    "click", stickyToggleButtonAction, false );
+  }
+  function stickyToggleButtonAction (zEvent) {
+    console.log("Toggling Sticky display;")
+    var notes = $(".stickyNote");
+    $.each(notes, forEachNote);
+
+    function forEachNote(i, note) {
+      var report = $(note.parentNode).find(".reportSheet")[0];
+      if (note.style.display != "inline-block") {
+        note.style.display = "inline-block";
+        report.style.display = "none";
+      } else {
+        note.style.display = "none";
+        report.style.display = "inline-block";
+      }
+    }
+  }
+
   function constructListingObject($, sheet) {
     var listingObj = {};
+    listingObj.monthlyCost = 0;
+    listingObj.photos = [];
+
     //Extract relevant info from sheet
     var listingBoxes = $(sheet).find('.form .legacyBorder div.formitem.formgroup.vertical');
     $.each(listingBoxes, parseListing);
 
     function parseListing(j, box) {
+      // Get photos
       if(j === 1) {
-        // Placeholder select for photo boxes later
-        //box.css('background','#ccffff')
-        //console.log(box);
-      }
+        var photoJson = box.childNodes[1].childNodes[1].dataset.multiPhotos;
+        var photoMap = JSON.parse(photoJson)["multi-photos"];
+        $.each(photoMap, trimPhotoURLs);
+
+        function trimPhotoURLs(p, photo) {
+          listingObj.photos.push(photo.url.split("&index")[0]);
+        }
+
+        listingObj.photoBoxClone = box.childNodes[1].cloneNode(true);
+      }// Extract address
       if(j === 3) {
-        // Extract address
         listingObj.address = box.innerText;
         listingObj.address = listingObj.address.replace("\n", " ");
-      }
+      }// Extract price
       if(j === 5) {
-        // Extract price
         listingObj.currentPrice = box.innerText;
         listingObj.currentPrice = listingObj.currentPrice.split(" ")[0];
         listingObj.currentPrice = listingObj.currentPrice.split("$")[1];
         listingObj.currentPrice = parseFloat(listingObj.currentPrice.replace(/,/g, ''));
-      }
+      }// Extract taxes
+      if(box.innerText.split(":")[0] === "Taxes") {
+        listingObj.taxes = box.innerText;
+        listingObj.taxes = listingObj.taxes.split("/")[0];
+        listingObj.taxes = listingObj.taxes.split("$")[1];
+        listingObj.taxes = parseFloat(listingObj.taxes.replace(/,/g, ''));
+      }// Extract type (Detatched, Condo, etc.)
       if(j === 9) {
-        // Extract type (Detatched, Condo, etc.)
         listingObj.type = box.children[0].innerText;
-      }
+      }// Extract room numbers
       if(j === 11) {
-        // Extract room numbers
-        listingObj.beds = box.children[1].innerText.split(":")[1];
         listingObj.baths = box.children[2].innerText.split(":")[1];
-      }
+
+        var numBeds = box.children[1].innerText.split(":")[1].split(" + ");
+        listingObj.mainBeds = numBeds[0];
+        if (numBeds.length > 1) {
+          listingObj.basementBeds = numBeds[1];
+        } else {
+          listingObj.basementBeds = 0;
+        }
+      }// Extract MLS#
       if(j === 12) {
-        // Extract MLS#
         listingObj.id = box.innerText;
         listingObj.id = listingObj.id.split(":")[1];
-      }
+      }// Process left info column
       if(j === 25) {
-        // Process left info column
         $.each(box.children, selectRelevantLeftColChildren);
 
         function selectRelevantLeftColChildren(k, child) {
@@ -115,6 +163,25 @@
           if (content[0] === "Apx Sqft") {
             listingObj.sqft = content[1];
           }
+          if (content[0] === "POTL Mo Fee") {
+            var fee = content[1].split("$");
+            if (fee.length > 1) {
+              listingObj.monthlyCost = parseFloat(fee[1].replace(/,/g, ''));
+            }
+          }
+        }
+      }// Process centre info column
+      if(j === 26) {
+        $.each(box.children[0].children, selectRelevantCentreColChildren);
+
+        function selectRelevantCentreColChildren(k, child) {
+          var content = child.innerText.split(":");
+          if (content[0] === "Maint") {
+            var fee = content[1].split("$");
+            if (fee.length > 1) {
+              listingObj.monthlyCost = parseFloat(fee[1].replace(/,/g, ''));
+            }
+          }
         }
       }
     }
@@ -122,10 +189,88 @@
   }
 
   function perListingUIChanges($, listingObj, sheet) {
-    if(listingObj.address) {
-      insertMapsLink($, sheet, util_removeUnitNumberFromAddress(listingObj.address));
+    var stickyNote = createStickyDiv($, listingObj);
+    var calculatedValues = calculateListingValues($, listingObj);
+
+    var stickyContents = generateStickyContents(listingObj, calculatedValues);
+    stickyNote.innerHTML = stickyContents;
+    sheet.parentElement.prepend(stickyNote);
+    // Add photoBox
+    var photoContainer = $('#photoContainer_' + listingObj.id);
+    addPhotoBox($, listingObj.photoBoxClone, listingObj.id, photoContainer);
+
+    // Scrub address for Google Maps
+    var mapsAddress = util_removeUnitNumberFromAddress(listingObj.address)
+
+    // Add a Google Maps link to the links section of the listing
+    insertMapsLink($, sheet, mapsAddress);
+    
+    // Add embeded map in sticky note
+    embedMap($, stickyNote, mapsAddress, listingObj.id);
+
+    // Add individual Toggle
+    var toggle = addIndividualToggle($, sheet, listingObj.id);
+
+  }
+  function createStickyDiv($, listingObj) {
+    var stickyNote = document.createElement('div');
+    stickyNote.id = "stickyNote_" + listingObj.id;
+    stickyNote.classList.add("stickyNote");
+    // Style the summary Sticky
+    GM_addStyle( 
+    ' #' + stickyNote.id + ' {' +
+    '    display: none;' +
+    '    background: white;' +
+    '    border: 0.5em solid;' +
+    '    padding: 1em;' +
+    '    width: 100%;' +
+    '    font-size: 1rem;' +
+    ' } ');
+    return stickyNote;
+  }
+
+  function calculateListingValues($, listingObj) {
+    var values = {};
+    values.mortgagePayment = 2740+48.53*(Math.ceil(listingObj.currentPrice/1000)/10-70);
+    values.maintenance = listingObj.type.includes("Condo") ? 
+                            listingObj.currentPrice*0.005/12 : 
+                            listingObj.currentPrice*0.01/12;
+    //calculate income potential
+    values.rent = 0;
+    if (listingObj.kitchens > 1) {
+      values.rent = 1200;
+      if (listingObj.basementBeds === 1) {
+        values.rent = 1400;
+      } else if (listingObj.basementBeds === 2) {
+        values.rent = 1800;
+      } else if (listingObj.basementBeds === 3) {
+        values.rent = 2200;
+      }
+    }
+    else if (listingObj.beds > 2) {
+      values.rent = 1000 * (listingObj.beds - 2);
     }
 
+    //calculate monthly net
+    values.netMonthly = values.mortgagePayment + listingObj.monthlyCost + values.maintenance - values.rent + (listingObj.taxes/12);
+    return values;
+  }
+
+  function generateStickyContents(listingObj, calculatedValues) {
+    return '<table><tr>' + 
+    '<td rowspan="3"><div id="photoContainer_' + listingObj.id + '"></div></td>' + 
+    '<td><b>$ ' + Math.ceil(listingObj.currentPrice/1000) + '<br>' + listingObj.type + '</b></td>' + 
+    '<td>Bedrooms: ' + listingObj.mainBeds + '</td>' + 
+    '<td>MLS: ' + listingObj.id + '</td>' + 
+    '</tr><tr>' + 
+    '<td><i>Rent: $ ' + calculatedValues.rent + '</i></td>' + 
+    '<td>Bathrooms: ' + listingObj.baths + '</td>' + 
+    '<td>' + listingObj.address + '</td>' + 
+    '</tr><tr>' + 
+    '<td>Net: $ ' + calculatedValues.netMonthly + '</td>' +
+    '<td><b>Basement: ' + listingObj.basement + '</b></td>' +  
+    '<td>Sq Ft: ' + listingObj.sqft + '</td>' + 
+    '</tr></table>';
   }
 
   function insertMapsLink($, sheet, address) {
@@ -166,4 +311,48 @@
     address = houseNumber + ' ' + address + ' ' + postcode;
     return address;
   }
+
+  function embedMap($, parentContainer, address, id) {
+    var iframeHTML1 = '<iframe src="https://maps.google.com/maps?&amp;q='
+    var iframeHTML2 = '&amp;output=embed" width="100%" height="450px" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>';
+    
+    var iframeCompleteHTML = iframeHTML1 + encodeURIComponent(address) + iframeHTML2;
+
+    var gmap = document.createElement('div');
+    gmap.id = "gmap_" + id;
+    gmap.width = "100%";
+    gmap.innerHTML = iframeCompleteHTML;
+
+    parentContainer.append(gmap);
+  }
+
+  function addPhotoBox($, clone, id, parentContainer) {
+    clone.setAttribute('id', parentContainer.id + "_photoBox");
+    parentContainer.append(clone);
+  }
+
+  function addIndividualToggle($, sheet, id) {
+    var toggle = document.createElement('div');
+    toggle.id = "toggleDiv_" + id;
+    toggle.innerHTML = '<button id="toggleButton_' + id + '" type="button">'
+                + 'Toggle</button>';
+    sheet.parentElement.prepend(toggle);
+    document.getElementById ("toggleButton_" + id).addEventListener (
+    "click", individualToggleButtonAction, false );
+  }
+  function individualToggleButtonAction (zEvent) {
+    var id = zEvent.target.id.split("_")[1];
+    console.log("Toggling " + id + " display;")
+    var note = $("#stickyNote_" + id)[0];
+    console.log(note);
+    var report = $(note.parentNode).find(".reportSheet")[0];
+    if (note.style.display != "inline-block") {
+      note.style.display = "inline-block";
+      report.style.display = "none";
+    } else {
+      note.style.display = "none";
+      report.style.display = "inline-block";
+    }
+  }
+
 })();
